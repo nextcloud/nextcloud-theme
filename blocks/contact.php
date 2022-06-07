86199f28f9f9b8cce9eef55ce207bc4c7606719e
Côme Chilliet <come.chilliet@nextcloud.com>
2022-06-07 16:46:17 +0200

    Remove commented out legacy code
    Signed-off-by: Côme Chilliet <come.chilliet@nextcloud.com>

--- blocks/contact.php
+++ blocks/contact.php
@@ -38,43 +38,6 @@ $text = get_field('text');
 			<div class="col-lg-6">
 				<div class="form-block">
 					<h4>Contact form</h4>
-					<!-- <div class="form-holder">
-						<form name="contact" method="post" action="">
-							<div class="input-holder">
-								<input type="text" name="yourname" maxlength="60" size="60" placeholder="Your name *" />
-							</div>
-							<div class="input-holder">
-								<input type="email" name="email" maxlength="80" size="60" placeholder="Your email *" />
-							</div>
-							<div class="input-holder">
-								<input type="text" name="organization" maxlength="100" size="60" placeholder="Name of your organization" />
-							</div>
-							<div class="input-holder">
-								<input type="text" name="phone" maxlength="40" size="60" placeholder="Phone number" />
-							</div>
-							<div class="input-holder selection hide-first">
-								<select id="foundnextcloud" name="foundnextcloud">
-									<option value="default">How did you learn about us?</option>
-									<option value="empty">Rather not say</option>
-									<option value="search">Search engine</option>
-									<option value="news">In the news</option>
-									<option value="recommendation">It was recommended to me</option>
-									<option value="usemyself">I use it privately</option>
-									<option value="advert">I saw it in an advertisement</option>
-									<option value="other">other</option>
-								</select>
-							</div>
-							<div class="input-holder">
-								<textarea name="comments" maxlength="2000" cols="60" rows="8" placeholder="Your message..."></textarea>
-							</div>
-							<div class="check-holder center white-text">
-								<input type="checkbox" id="gdprcheck" name="gdprcheck" value="gdprchecked"><label for="gdprcheck">I agree to the <a href="#">Terms of Service</a></label>
-							</div>
-							<div class="submit-holder center">
-								<input class="" type="submit" value="Submit">
-							</div>
-						</form>
-					</div> -->
 					<?php echo do_shortcode('[contact-form-7 id="15865" title="Contact new hip"]');  ?>
 					<h6 class="info-text">Support questions through this form will get ignored.</h6>
 				</div>
