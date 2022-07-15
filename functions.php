b4cd070319a390409b4de3fb6a18b03c23d889fb
Carl Schwan <carl@carlschwan.eu>
2022-07-15 10:46:56 +0200

    Disable dns prefetch
    Signed-off-by: Carl Schwan <carl@carlschwan.eu>

--- functions.php
+++ functions.php
@@ -1126,3 +1126,20 @@ function wpml_floating_language_switcher() {
        do_action('wpml_add_language_selector');
    echo '</div>'; 
 }
+
+function disable_wp_emojicons() {
+
+  // all actions related to emojis
+  remove_action( 'admin_print_styles', 'print_emoji_styles' );
+  remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
+  remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
+  remove_action( 'wp_print_styles', 'print_emoji_styles' );
+  remove_filter( 'wp_mail', 'wp_staticize_emoji_for_email' );
+  remove_filter( 'the_content_feed', 'wp_staticize_emoji' );
+  remove_filter( 'comment_text_rss', 'wp_staticize_emoji' );
+
+  // filter to remove TinyMCE emojis
+  add_filter( 'tiny_mce_plugins', 'disable_emojicons_tinymce' );
+}
+add_action( 'init', 'disable_wp_emojicons' );
+remove_action('wp_head', 'wp_resource_hints', 2)
