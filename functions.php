30e6cd5e2952081d672708f67e76877c9030436c
peterwebdesign <info@petermocanu.com>
2022-08-23 16:33:32 +0200

    fix codestyle
    Signed-off-by: peterwebdesign <info@petermocanu.com>

--- functions.php
+++ functions.php
@@ -1118,28 +1118,28 @@ function my_wp_nav_menu_objects($items, $args) {
 	return $items;
 }
 
-add_action('wp_footer', 'wpml_floating_language_switcher'); 
+add_action('wp_footer', 'wpml_floating_language_switcher');
 
-function wpml_floating_language_switcher() { 
-   echo '<div class="wpml-floating-language-switcher">';
-       //PHP action to display the language switcher (see https://wpml.org/documentation/getting-started-guide/language-setup/language-switcher-options/#using-php-actions)
-       do_action('wpml_add_language_selector');
-   echo '</div>'; 
+function wpml_floating_language_switcher() {
+	echo '<div class="wpml-floating-language-switcher">';
+	//PHP action to display the language switcher (see https://wpml.org/documentation/getting-started-guide/language-setup/language-switcher-options/#using-php-actions)
+	do_action('wpml_add_language_selector');
+	echo '</div>';
 }
 
 function disable_wp_emojicons() {
 
   // all actions related to emojis
-  remove_action( 'admin_print_styles', 'print_emoji_styles' );
-  remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
-  remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
-  remove_action( 'wp_print_styles', 'print_emoji_styles' );
-  remove_filter( 'wp_mail', 'wp_staticize_emoji_for_email' );
-  remove_filter( 'the_content_feed', 'wp_staticize_emoji' );
-  remove_filter( 'comment_text_rss', 'wp_staticize_emoji' );
+	remove_action('admin_print_styles', 'print_emoji_styles');
+	remove_action('wp_head', 'print_emoji_detection_script', 7);
+	remove_action('admin_print_scripts', 'print_emoji_detection_script');
+	remove_action('wp_print_styles', 'print_emoji_styles');
+	remove_filter('wp_mail', 'wp_staticize_emoji_for_email');
+	remove_filter('the_content_feed', 'wp_staticize_emoji');
+	remove_filter('comment_text_rss', 'wp_staticize_emoji');
 
-  // filter to remove TinyMCE emojis
-  add_filter( 'tiny_mce_plugins', 'disable_emojicons_tinymce' );
+	// filter to remove TinyMCE emojis
+	add_filter('tiny_mce_plugins', 'disable_emojicons_tinymce');
 }
-add_action( 'init', 'disable_wp_emojicons' );
+add_action('init', 'disable_wp_emojicons');
 remove_action('wp_head', 'wp_resource_hints', 2);
